e387533a9b8299d57d9dcb30f7ee0096e5636f81
chukwu obed <codingwithsmart@gmail.com>
2024-08-02 14:42:06 -1100

    Add main.js file implemented intersection  observer API for sticky header functionality Edited the hero section to fix image overflow and overlay

---
+++ main.js
@@ -0,0 +1,19 @@
+const sectionEl = document.querySelector(".section-hero");
+
+
+const check = new IntersectionObserver(
+    (entries) => {
+        const entry = entries[0];
+        if (!entry.isIntersecting) {
+            document.body.classList.add("sticky")
+        } else {
+            document.body.classList.remove("sticky")
+        }
+    },
+    {
+        root: null,
+        threshold: 0,
+        rootMargin: "-80px"
+    })
+
+check.observe(sectionEl)
