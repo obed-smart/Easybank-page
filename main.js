ca1bcf5c4729b6d53db98c775e22d2d90b696e4d
chukwu obed <codingwithsmart@gmail.com>
2024-08-06 21:46:27 -1100

    Add menu navigation and overlay to the body

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 const sectionEl = document.querySelector(".section-hero");
+const navButton = document.getElementById("nav-btn");
+const navLinks = document.querySelectorAll("a:link");
+const header = document.querySelector(".header")
+
+// Add click event to the menu button
+
+
+
+navButton.addEventListener("click", () => {
+    document.body.classList.toggle("open-nav");
+    document.body.classList.toggle("overlay")
+})
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        if (link.classList.contains("nav__link")) {
+            document.body.classList.toggle("open-nav")
+            document.body.classList.toggle("overlay")
+
+        }
+    })
+})
+
+
 
 
 const check = new IntersectionObserver(
@@ -12,7 +36,8 @@ const check = new IntersectionObserver(
     },
     {
         root: null,
-        threshold: 0.1,
+        threshold: 0,
+        rootMargin: "-80px"
     })
 
 check.observe(sectionEl)
